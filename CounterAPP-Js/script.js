8fca7a79e869fca90af7664f5459ac5d5e12fc28
fahadkarim <fahadkar33m@icloud.com>
2026-02-20 23:00:35 +0500

    coun tracker project using js

---
+++ CounterAPP-Js/script.js
@@ -0,0 +1,17 @@
+const count = document.querySelector('.counter');
+const minusBtn = document.querySelector('.minus-btn');
+const plusBtn = document.querySelector('.plus-btn');
+const changeBy = document.querySelector('.changeBy');
+
+plusBtn.addEventListener('click', () => {
+    const countValue = parseInt(count.innerText);
+    const changeByValue = parseInt(changeBy.value);
+    count.innerText = countValue + changeByValue;
+});
+
+minusBtn.addEventListener('click', () => {
+    const countValue = parseInt(count.innerText);
+    const changeByValue = parseInt(changeBy.value);
+    count.innerText = countValue - changeByValue;
+});
+
