015ccf517811024f5634c33a4184d9109473abf8
fahadkarim <fahadkar33m@icloud.com>
2026-03-26 18:31:34 +0500

    pop up and analogue project

---
+++ 22-Pop-up/script.js
@@ -0,0 +1,11 @@
+let popUp = document.getElementById("popup");
+
+function openPopup(){
+    popUp.classList.add("open-popup"); // removed the dot
+}
+
+function closePopup(){
+    popUp.classList.remove("open-popup"); // removed the dot
+}
+
+
